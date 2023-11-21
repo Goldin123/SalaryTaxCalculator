ef4549a3da79055d1a291517837b47c01cf88645
Goldin Baloyi <GoldinB@hollywoodbets.net>
2023-11-21 20:46:00 +0200

    added response constructor

--- IncomeTaxCalculator.Model/TaxSalaryResponse.cs
+++ IncomeTaxCalculator.Model/TaxSalaryResponse.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿// Ignore Spelling: UIF
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,10 +10,19 @@ namespace IncomeTaxCalculator.Model
 {
     public class TaxSalaryResponse
     {
-        public decimal GrossAmount { get; set; }
-        public decimal NettAmount { get; set; }
-        public decimal TaxAmount { get; set; }
-        public decimal UIF { get; set; }
-        public decimal TaxPercentage { get; set; }
+        public decimal? GrossAmount { get; set; }
+        public decimal? NettAmount { get; set; }
+        public decimal? TaxAmount { get; set; }
+        public decimal? UIF { get; set; }
+        public decimal? TaxPercentage { get; set; }
+
+        public TaxSalaryResponse(decimal? grossAmount, decimal? nettAmount, decimal? taxAmount, decimal? uif, decimal? taxPercentage)
+        {
+            GrossAmount = grossAmount;
+            NettAmount = nettAmount;
+            TaxAmount = taxAmount;
+            UIF = uif;
+            TaxPercentage = taxPercentage;
+        }
     }
 }
